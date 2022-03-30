c331d09eb10aca36c099668612eeeeae77491ec4
ace95322 <ace95322@gmail.com>
2022-03-30 12:59:44 +0530

    - fix home page issue

--- app/Helpers/UserHelper.php
+++ app/Helpers/UserHelper.php
@@ -9,6 +9,8 @@ class UserHelper
 
     public static function getIsUserPlanTypeFree(){
         if(isset(auth('api')->user()->is_admin) && $user = auth('api')->user()->is_admin != 1){
+            $user = auth('api')->user();
+            $user->load('active_subscription');
             $sub = $user->active_subscription()->first();
             Log::info("Active plans => ".print_r($sub->plan->free, true));
             return $sub->plan->free;
